3442415310a7ec151901210cd2f9b0d7dc6c51f2
yukiforwork <yukiforwork@gmail.com>
2022-02-20 09:49:29 +0000

    username押下後遷移修正

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -7,7 +7,7 @@
                 <div class="media-body">
                     <div>
                         {{-- 投稿の所有者のユーザ詳細ページへのリンク --}}
-                        {!! link_to_route('users.show', $micropost->user->name, ['user' => $micropost->user->name]) !!}
+                        {!! link_to_route('users.show', $micropost->user->name, ['user' => $micropost->user->id]) !!}
                         <span class="text-muted">posted at {{ $micropost->created_at }}</span>
                     </div>
                     <div>
